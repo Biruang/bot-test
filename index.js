103ed248bafac47428182ea5d9d761200cfe7e9b
Biruang <saidovte@gmail.com>
2023-05-14 09:08:36 +0600

    feat: add init

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import axios from "axios";
+import { log } from "console";
 import {config} from 'dotenv';
 import express from "express";
 
 config();
-const app = express();
+const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.API_TOKEN}`;
 
+//Check webhook status before deploy
+const status = await axios.get(`${TELEGRAM_URI}/getWebhookInfo`);
+if(!status?.data || !status?.data.result.url) {
+    log(`Setting webhook on url:${HOST_URL}`);
+    const res = await axios.post(`${TELEGRAM_URI}/setWebhook`, `url=${HOST_URL}/message`)
+    log(res.data);
+}
+
+const app = express();
 app.use(express.json())
 app.use(
     express.urlencoded({
@@ -12,9 +22,7 @@ app.use(
     })
 )
 
-const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.API_TOKEN}`
-
-app.post("/new", async (req, res) => {
+app.post("/message", async (req, res) => {
     const {message} = req.body;
 
     const text = message?.text?.toLowerCase().trim();
